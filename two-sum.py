6402a5e08f3c9bd04826a2d17c65c6e05dbbae3c
mjzalewska <m.jarzebska.zalewska@gmail.com>
2023-11-14 16:53:48 +0100

    Corrected solution

--- two-sum.py
+++ two-sum.py
@@ -1,9 +1,5 @@
 def twoSum(nums: list[int], target: int)-> list[int]:
-    solution = []
-    for i in nums:
-        for j in nums[nums.index(i):]:
-            if nums.index(i)!=nums.index(j) and (i + j == target):
-                solution.extend([nums.index(i),nums.index(j)])
-    return solution
+
 
 print(twoSum([1,2,3,4], 6))
+
