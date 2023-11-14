0d852354ba05e3f8eab1ea4689531047454604ed
mjzalewska <64257298+mjzalewska@users.noreply.github.com>
2023-11-14 16:55:40 +0100

    Adding final solution

--- two-sum.py
+++ two-sum.py
@@ -1,5 +1,16 @@
 def twoSum(nums: list[int], target: int)-> list[int]:
+    solution = []
+    if len(nums) < 2:
+                return []
+    elif len(nums) == 2:
+                solution.extend([0, 1])
+    else:
+            for i in range(len(nums)):
+                for j in range(i, len(nums)):
+                    if nums[i] + nums[j] == target and  i != j:
+                        solution.extend([i, j])
+    return solution
 
-
+print(twoSum([3, 3], 6))
 print(twoSum([1,2,3,4], 6))
 
